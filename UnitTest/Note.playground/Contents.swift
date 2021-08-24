dde87f3c3c21d7493b0e298ddfa0ba8cbc7d4c31
TDCIAN <tdcian71@gmail.com>
2021-08-24 17:48:06 +0900

    the first principle

---
+++ UnitTest/Note.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// MARK: The F.I.R.S.T Principle (Section 2.5)
+/*
+Fast - Unit tests run fast
+Independent - Unit tests are independent
+Repeatable - Unit tests are repeatable
+Self-validating - Unit test validates itself
+Thorough & Timely - Cover edge cases
+ */
