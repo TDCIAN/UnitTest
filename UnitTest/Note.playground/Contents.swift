87564f4f67b0073212089dbc78365808626f3054
TDCIAN <tdcian71@gmail.com>
2021-08-25 13:55:41 +0900

    create a new test case class

--- UnitTest/Note.playground/Contents.swift
+++ UnitTest/Note.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// MARK: Section 2: Basics of Unit Testing in Xcode
+
 // MARK: The F.I.R.S.T Principle (Section 2.5)
 /*
 Fast - Unit tests run fast
@@ -7,4 +9,21 @@ Independent - Unit tests are independent
 Repeatable - Unit tests are repeatable
 Self-validating - Unit test validates itself
 Thorough & Timely - Cover edge cases
+ */
+
+// MARK: Test-Driven Development Lifecycle (Section 2.6)
+/*
+ The Test-Driven Development Life Cycle (Red -> Green -> Refactor -> Repeat)
+ 
+    - Red
+        - Write Unit test that fails.
+ 
+    - Green
+        - Write App code to make Unit test pass.
+ 
+    - Refactor
+        - Clean up and improve Unit test and app code.
+ 
+    - Repeat
+        - Repeat these steps until all your app features are built and tested.
  */
